137418859f36113954ef58c6d8dac558ddf9a252
Ahmad Ardiansyah <ahmad.ardiansyah@mceasy.co.id>
2025-10-08 15:25:50 +0700

    Bundling the tools

--- dist/index.js
+++ dist/index.js
@@ -27247,35 +27247,22 @@ function requireCore () {
 var coreExports = requireCore();
 
 /**
- * Waits for a number of milliseconds.
- *
- * @param milliseconds The number of milliseconds to wait.
- * @returns Resolves with 'done!' after the wait is over.
- */
-async function wait(milliseconds) {
-    return new Promise((resolve) => {
-        if (isNaN(milliseconds))
-            throw new Error('milliseconds is not a number');
-        setTimeout(() => resolve('done!'), milliseconds);
-    });
-}
-
-/**
  * The main function for the action.
  *
  * @returns Resolves when the action is complete.
  */
 async function run() {
     try {
-        const ms = coreExports.getInput('milliseconds');
+        const name = coreExports.getInput('name');
+        const language = coreExports.getInput('language');
         // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-        coreExports.debug(`Waiting ${ms} milliseconds ...`);
-        // Log the current timestamp, wait, then log the new timestamp
-        coreExports.debug(new Date().toTimeString());
-        await wait(parseInt(ms, 10));
-        coreExports.debug(new Date().toTimeString());
+        coreExports.debug(`Name      : ${name}`);
+        coreExports.debug(`Language  : ${language}`);
+        const docker = 'FROM node:lts-alpine\nWORKDIR /app\nCOPY . .\nEXPOSE 3000';
+        const k8s = `apiVersion: apps/v1\nkind: Deployment\nmetadata:\n  name: ${name}\n  labels:\n    app: ${name}\nspec:\n  replicas: 1\n  selector:\n    matchLabels:\n      app: ${name}\n  template:\n    metadata:\n      labels:\n        app: ${name}\n    spec:\n      containers:\n        - name: ${name}\n          image: nginx:1.14.2\n          ports:\n            - containerPort: 80`;
         // Set outputs for other workflow steps to use
-        coreExports.setOutput('time', new Date().toTimeString());
+        coreExports.setOutput('docker', docker);
+        coreExports.setOutput('k8s', k8s);
     }
     catch (error) {
         // Fail the workflow run if an error occurs
